ccdc87ea2fb431bb69dc8bd7926cddf9b758862b
CheremnykhYA <yurchest@gmail.com>
2020-04-17 03:38:53 +0300

    code : decomposition

--- main.cpp
+++ main.cpp
@@ -5,75 +5,69 @@ using namespace std;
 
 
 
-
-int main()
-{
-     const size_t SCREEN_WIDTH = 80;
-    size_t max_asterisk = SCREEN_WIDTH - 4;
-
-    size_t number_count;
-    cerr<<"Enter number count ";
-    cin>>number_count;
-
-    vector<double> numbers(number_count);
-
-
-    cerr<<"Enter massive"<<endl;
-    for(size_t i = 0;i < number_count; i++){
-        cin>>numbers[i];
+vector<double>
+input_numbers(size_t count) {
+    vector<double> result(count);
+    for (size_t i = 0; i < count; i++) {
+        cin >> result[i];
     }
+    return result;
+}
 
-    size_t bin_count;
-    cerr<<"Enter bin count ";
-    cin>>bin_count;
+
+void
+find_minmax(const vector<double>& numbers, double& min, double& max){
+    min = numbers[0];
+    for (double x : numbers) {
+        if (x < min) {
+            min = x;
+        }
+        else if (x > max) {
+            max = x;
+        }
+    }
+}
+
+
+vector<size_t>
+make_histogram(const vector<double>& numbers, size_t& bin_count){
 
     vector<size_t> bins(bin_count, 0);
-
-    //Ïîèñê max,min
-    double min = numbers[0];
-    double max = numbers[0];
-    for (double x : numbers) {
-            if (x < min) {
-                min = x;
-            }
-            else if (x > max) {
-                max = x;
-            }
-    }
-
-
-
+    double min, max;
+    find_minmax(numbers, min, max);
     for (double x : numbers){
-    size_t bin_index = (size_t)(x - min)* bin_count / (max- min);
-
-
-    if (bin_index == bin_count){
-        bin_index--;
-    }
-
+        size_t bin_index = (size_t)((x - min)/(max - min) * bin_count);
+        if (bin_index == bin_count){
+            bin_index--;
+        }
     bins[bin_index]++;
     }
+    return bins;
+}
 
-    /*
-    for(size_t b:bins)
-    cout<<b;
-    */
-      double max_count = bins[0];
+void
+show_histogram_text(const vector<size_t>& bins){
 
-      for(size_t bin : bins){
-         if (bin > max_count) {
-                max_count = bin;
-            }
-         }
+    double max_count = bins[0];
+    for(size_t bin : bins){
+        if (bin > max_count) {
+            max_count = bin;
+        }
+     }
 
-         if (max_count < 10) const int max_asterisk = SCREEN_WIDTH - 1 - 1;
-         if (max_count >= 10 && max_count < 100) max_asterisk = SCREEN_WIDTH - 1 - 2;
-         if (max_count >= 100) max_asterisk = SCREEN_WIDTH - 1 - 3;
+     const size_t SCREEN_WIDTH = 80;
+     size_t max_asterisk;
+     if (max_count < 10)  max_asterisk = SCREEN_WIDTH - 1 - 1;
+     if (max_count >= 10 && max_count < 100) max_asterisk = SCREEN_WIDTH - 1 - 2;
+     if (max_count >= 100) max_asterisk = SCREEN_WIDTH - 1 - 3;
 
-          double koeff = 1;
-             if (max_count > max_asterisk){
-         koeff = (double)(max_asterisk)/max_count;
-         }
+      double koeff = 1;
+         if (max_count > max_asterisk){
+     koeff = (double)(max_asterisk)/max_count;
+     }
+
+
+
 
     for(size_t bin : bins){
 
@@ -96,16 +90,30 @@ int main()
             cout<<"*";}
         cout<<endl;
         }
-/*
-        size_t number;
-        double sr;
-        vector<size_t> massive(number);
+    return;
+}
 
-        for(size_t x : massive){
-            sr=sr+x;
-        }
 
-        sr = sr/number;
-*/
+int main()
+{
+
+
+    size_t number_count;
+    cerr<<"Enter number count ";
+    cin>>number_count;
+
+    cerr<<"Enter massive"<<endl;
+    const auto numbers = input_numbers(number_count);
+
+    size_t bin_count;
+    cerr<<"Enter bin count ";
+    cin>>bin_count;
+
+    //MAKE HISTOGRAM
+    const auto bins = make_histogram(numbers, bin_count);
+
+    //SHOW HISTOGRAM
+    show_histogram_text(bins);
+
     return 0;
 }
